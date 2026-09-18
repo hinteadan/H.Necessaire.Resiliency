ad3997441ead5f3ec72f16797f64dbf4c5a0e3aa
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2026-09-18 15:43:08 +0300

    OD: Scaffolding

---
+++ src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.CLI/HAppConfig.cs
@@ -0,0 +1,48 @@
+﻿using H.Necessaire.Runtime;
+using System.Reflection;
+
+namespace H.Necessaire.Resiliency.CLI
+{
+    internal static class HAppConfig
+    {
+        const string srcFolderRelativePath = "/src/H.Necessaire.Resiliency/";
+
+        public static ImAnApiWireup WithDefaultRuntimeConfig(this ImAnApiWireup wireup)
+        {
+            return
+                wireup
+                .With(x => x.Register<RuntimeConfig>(() => new RuntimeConfig
+                {
+                    Values = new[] {
+                        "NuSpecRootFolderPath".ConfigWith(GetCodebaseFolderPath()),
+                    },
+                }));
+            ;
+        }
+
+        static string ReadConnectionStringFromFile(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            if (!fileInfo.Exists)
+                return null;
+
+            if (!HSafe.Run(() => File.ReadAllText(fileInfo.FullName)).Ref(out var res, out var result))
+                return null;
+
+            return result.NullIfEmpty();
+        }
+
+        static string GetCodebaseFolderPath()
+        {
+            string codeBase = Assembly.GetExecutingAssembly()?.Location ?? string.Empty;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string dllPath = Uri.UnescapeDataString(uri.Path);
+            int srcFolderIndex = dllPath.ToLowerInvariant().IndexOf(srcFolderRelativePath, StringComparison.OrdinalIgnoreCase);
+            if (srcFolderIndex < 0)
+                return string.Empty;
+            string srcFolderPath = Path.GetDirectoryName(dllPath.Substring(0, srcFolderIndex + srcFolderRelativePath.Length)) ?? string.Empty;
+            return srcFolderPath;
+        }
+    }
+}
