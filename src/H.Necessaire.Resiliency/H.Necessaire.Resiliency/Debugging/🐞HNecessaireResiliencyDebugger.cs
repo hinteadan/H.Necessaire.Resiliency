aae03b8361c1842b98edf62a33032ffda5b66705
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-24 01:03:36 +0300

    OD: RavenDB Resiliency and H.Necessaire.Resiliency refinements

--- src/H.Necessaire.Resiliency/H.Necessaire.Resiliency/Debugging/🐞HNecessaireResiliencyDebugger.cs
+++ src/H.Necessaire.Resiliency/H.Necessaire.Resiliency/Debugging/🐞HNecessaireResiliencyDebugger.cs
@@ -1,6 +1,4 @@
 ﻿using H.Necessaire.Resiliency.Abstractions;
-using H.Necessaire.Resiliency.DataModels;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,20 +18,6 @@ namespace H.Necessaire.Resiliency.Debugging
 
         public async Task Debug()
         {
-            var x = (-5d).FlipIntervalValueToOppositeEnd(NumberInterval.Percent);
-
-            var val = new double[] {
-                1d.FlipIntervalValueToOppositeEnd(NumberInterval.Percent),
-                99d.FlipIntervalValueToOppositeEnd(NumberInterval.Percent),
-                150d.FlipIntervalValueToOppositeEnd(NumberInterval.Percent),
-                100d.FlipIntervalValueToOppositeEnd(NumberInterval.Percent),
-                (-5d).FlipIntervalValueToOppositeEnd(NumberInterval.Percent),
-                1d.FlipIntervalValueToOppositeEnd(HResiliencyMeasurement.Myriad),
-                5_000d.FlipIntervalValueToOppositeEnd(HResiliencyMeasurement.Myriad),
-                2_000d.FlipIntervalValueToOppositeEnd(HResiliencyMeasurement.Myriad),
-            };
-            return;
-
             using (var _t = await log.LogInfoDuration(nameof(HNecessaireResiliencyDebugger), "🐞"))
             {
                 (await resiliencyTestServiceProvider.GetAllServiceInstancesOrderedByResiliencyWithIDs()).Ref(out var res, out var services);
