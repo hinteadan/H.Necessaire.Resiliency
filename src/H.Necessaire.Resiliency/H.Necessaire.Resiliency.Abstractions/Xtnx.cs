d3e0ea538dcae98bde6d5c664f8d3277b3b7253a
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-23 14:20:05 +0300

    OD: Refine API

--- src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.Abstractions/Xtnx.cs
+++ src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.Abstractions/Xtnx.cs
@@ -4,11 +4,15 @@ namespace H.Necessaire.Resiliency.Abstractions
 {
     public static class Xtnx
     {
-        public static T EnsureMinMax<T>(this T value, T min, bool isMinIncludedAsValid, T max, bool isMaxIncludedAsValid, T valueIfLessThanMin, T valueIfGreaterThanMax)
-            where T : IComparable<T>
+        public static T EnsureMinMaxInclusive<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: true, valueIfLessThanMin, valueIfGreaterThanMax);
+        public static T EnsureMinMaxExclusive<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: false, max, isMaxIncludedAsValid: false, valueIfLessThanMin, valueIfGreaterThanMax);
+
+        public static T EnsureMinMax<T>(this T value, T min, bool isMinIncludedAsValid, T max, bool isMaxIncludedAsValid, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
         {
-            bool isValueTooLow = !(isMinIncludedAsValid ? value.CompareTo(min) >= 0 : value.CompareTo(min) > 0);
-            bool isValueTooHigh = !(isMaxIncludedAsValid ? value.CompareTo(max) <= 0 : value.CompareTo(max) < 0);
+            bool isValueTooLow = !(isMinIncludedAsValid ? value?.CompareTo(min) >= 0 : value?.CompareTo(min) > 0);
+            bool isValueTooHigh = !(isMaxIncludedAsValid ? value?.CompareTo(max) <= 0 : value?.CompareTo(max) < 0);
             bool isValueValid = !isValueTooLow && !isValueTooHigh;
             if (isValueValid)
                 return value;
