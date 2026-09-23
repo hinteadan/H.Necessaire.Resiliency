1a3dc94dd57e5d1b2b2df6e7b075aeea30c49177
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-23 13:50:31 +0300

    OD: Refine API

---
+++ src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.Abstractions/Xtnx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace H.Necessaire.Resiliency.Abstractions
+{
+    public static class Xtnx
+    {
+        static TimeSpan EnsureMinMax(this TimeSpan value, TimeSpan min, bool isMinIncludedAsValid, TimeSpan max, bool isMaxIncludedAsValid, TimeSpan valueIfLessThanMin,TimeSpan valueIfGreaterThanMax)
+        {
+            bool isValueTooLow = !(isMinIncludedAsValid ? value >= min : value > min);
+            bool isValueTooHigh = !(isMaxIncludedAsValid ? value <= max : value < max);
+            bool isValueValid = !isValueTooLow && !isValueTooHigh;
+            if (isValueValid)
+                return value;
+            if (isValueTooLow)
+                return valueIfLessThanMin;
+            if (isValueTooHigh)
+                return valueIfGreaterThanMax;
+            return value;//Should never get here
+        }
+    }
+}
