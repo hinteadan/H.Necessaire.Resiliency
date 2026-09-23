0a50fbe793b52686488c345892d5f0298a6e719a
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2026-09-23 14:55:58 +0300

    OD: Refine API

--- src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.Abstractions/Xtnx.cs
+++ src/H.Necessaire.Resiliency/H.Necessaire.Resiliency.Abstractions/Xtnx.cs
@@ -4,12 +4,26 @@ namespace H.Necessaire.Resiliency.Abstractions
 {
     public static class Xtnx
     {
-        public static T EnsureMinMaxInclusive<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+        public static T EnsureMinMax<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
             => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: true, valueIfLessThanMin, valueIfGreaterThanMax);
         public static T EnsureMinMaxExclusive<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
             => value.EnsureMinMax(min, isMinIncludedAsValid: false, max, isMaxIncludedAsValid: false, valueIfLessThanMin, valueIfGreaterThanMax);
+        public static T EnsureMinLessThanMax<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: false, valueIfLessThanMin, valueIfGreaterThanMax);
+        public static T EnsureMaxGreaterThanMin<T>(this T value, T min, T max, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: false, max, isMaxIncludedAsValid: true, valueIfLessThanMin, valueIfGreaterThanMax);
 
-        public static T EnsureMinMax<T>(this T value, T min, bool isMinIncludedAsValid, T max, bool isMaxIncludedAsValid, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
+        public static T EnsureMinMax<T>(this T value, T min, T max, T valueIfLessThanMin) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: true, valueIfLessThanMin, valueIfGreaterThanMax: max);
+        public static T EnsureMinLessThanMax<T>(this T value, T min, T max, T valueIfGreaterThanMax) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: false, valueIfLessThanMin: min, valueIfGreaterThanMax);
+        public static T EnsureMaxGreaterThanMin<T>(this T value, T min, T max, T valueIfLessThanMin) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: false, max, isMaxIncludedAsValid: true, valueIfLessThanMin, valueIfGreaterThanMax: max);
+
+        public static T EnsureMinMax<T>(this T value, T min, T max) where T : IComparable<T>
+            => value.EnsureMinMax(min, isMinIncludedAsValid: true, max, isMaxIncludedAsValid: true, valueIfLessThanMin: min, valueIfGreaterThanMax: max);
+
+        static T EnsureMinMax<T>(this T value, T min, bool isMinIncludedAsValid, T max, bool isMaxIncludedAsValid, T valueIfLessThanMin, T valueIfGreaterThanMax) where T : IComparable<T>
         {
             bool isValueTooLow = !(isMinIncludedAsValid ? value?.CompareTo(min) >= 0 : value?.CompareTo(min) > 0);
             bool isValueTooHigh = !(isMaxIncludedAsValid ? value?.CompareTo(max) <= 0 : value?.CompareTo(max) < 0);
